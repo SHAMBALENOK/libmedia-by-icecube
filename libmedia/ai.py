c32dbe0e3ab2b5f45040ccd8e648596fc420cf00
Ice Cube <166421956+SHAMBALENOK@users.noreply.github.com>
2025-02-18 17:20:19 +0200

    Add files via upload

--- libmedia/ai.py
+++ libmedia/ai.py
@@ -1,12 +1,21 @@
+from asyncio import WindowsSelectorEventLoopPolicy
+
 from g4f.client import Client
+import json
+import time
+import asyncio
+
+asyncio.set_event_loop_policy(WindowsSelectorEventLoopPolicy())
 
 
 def get_movie(promt):
     client = Client()
     response = client.chat.completions.create(
         model="qwen-2.5-coder-32b",
-        messages=[{"role": "user", "content": f"Можешь ли ты мне дать список фильмов и сериалов, с следующим описанием {promt}, "
-                                              f"leaving only name, in the one string"}],
+        messages=[{"role": "user",
+                   "content": f"Можешь ли ты мне дать список фильмов и сериалов, с следующим описанием {promt}, "
+                              f"когда будешь выписывать фильмы оставь лишь названия, и выпиши их в "
+                              f"строку"}],
         web_search=False
     )
     # print(response.choices[0].message.content)
@@ -14,4 +23,33 @@ def get_movie(promt):
     return response.choices[0].message.content
 
 
-print(get_movie('film about future '))
+def get_desc(path):
+    listy = []
+    file = open(path, 'r')
+    items = json.load(file)
+    for item in items:
+        info = {'name': item['title'], 'date': item['release_date']}
+        listy.append(info)
+
+    movies = []
+    for item in listy:
+        name = item['name']
+        date = item['date'].replace('Released', '')
+        client = Client()
+        response = client.chat.completions.create(
+            model="qwen-2.5-coder-32b",
+            messages=[{"role": "user", "content": f'Привет можешь дать мне описание фильма под названием {name}, '
+                                                  f'вышедшего в {date}, лимит символов: 200'}],
+            web_search=False
+        )
+        info = {'name': name, 'date': date, 'description': response.choices[0].message.content.replace('Конечно! ', '')}
+        movies.append(info)
+        print(info)
+    # print(response.choices[0].message.content)
+    return movies
+
+
+data = get_desc('movie_info.json')
+print(data)
+with open("db.txt", "w") as file:
+    file.write(data)
--- libmedia/main.py
+++ libmedia/main.py
@@ -1,22 +1,31 @@
 from database import *
 from sqlalchemy.orm import Session
-from fastapi import Depends, FastAPI, Body
+from fastapi import Depends, FastAPI, Body, Request
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import JSONResponse, FileResponse
+from fastapi.responses import JSONResponse, FileResponse, HTMLResponse
 
 app = FastAPI()
 
-app.mount("/public", StaticFiles(directory="public", html=True))
-
-# определяем зависимость
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+app.mount('/public', StaticFiles(directory='public'))
 
 
 @app.get("/")
-def main():
+async def root():
     return FileResponse("public/index.html")
+
+
+@app.post("/api/search")
+async def search1(text=Body()):
+    t = text['text']
+    return t
+
+
+# @app.post("/button_click")
+# async def button_click(request: Request):
+#     print("Кнопка нажата!")
+#
+#
+# @app.post("/submit_text")
+# async def submit_text(text: str):
+#     print(f"Текст из input: {text}")
+#     return {"message": f"Текст '{text}' успешно передан!"}
