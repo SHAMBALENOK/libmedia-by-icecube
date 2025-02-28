e63165acaf67bae99c8bb3b6c138d252975f3463
Ice Cube <166421956+SHAMBALENOK@users.noreply.github.com>
2025-02-28 17:18:07 +0200

    Delete Проекты/libmedia-by-icecube-main directory

--- Проекты/libmedia-by-icecube-main/libmedia/translation.py
+++
@@ -1,7 +0,0 @@
-from translate import Translator
-
-def translate(text):
-    translator = Translator(from_lang='en', to_lang='ru')
-    res = translator.translate(text)
-    print(res)
-    return res
